5425ac58ee9ecd549aeca5337c7dd0f279359c01
Anurag Gupta <anurag281996@gmail.com>
2020-11-29 23:05:30 +0530

    12 ms

---
+++ solutions/350. Intersection of Two Arrays II.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        unordered_map <int, int> m;
+        for(auto x:nums1)
+            m[x]++;
+        for(auto x:nums2) {
+            if(m[x]-- > 0){
+                ans.push_back(x);
+            }
+        }
+        return ans;
+    }
+};
