6a28bdd429b720445aa3ce110ebe2488f8b3a224
Anurag Gupta <anurag281996@gmail.com>
2020-12-05 03:39:17 +0530

    4 ms

---
+++ solutions/69. Sqrt(x).cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int left=1, right=x, mid;
+        
+        if(x<2)
+            return x;
+        
+        while(left<right) {    
+            mid = left + (right-left)/2;
+            if(mid==x/mid)
+                return mid;
+            else if(mid>x/mid) {
+                right=mid;
+            } else {
+                left=mid+1;
+            }
+        }
+        
+        return left-1;
+    }
+};
