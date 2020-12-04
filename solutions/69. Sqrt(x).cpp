5fbc530a6e6c7135ad77e145421861dc8770cb9a
Anurag Gupta <anurag281996@gmail.com>
2020-12-05 03:56:38 +0530

    4 ms

--- solutions/69. Sqrt(x).cpp
+++ solutions/69. Sqrt(x).cpp
@@ -6,17 +6,30 @@ public:
         if(x<2)
             return x;
         
-        while(left<right) {    
+//         while(left<right) {    
+//             mid = left + (right-left)/2;
+//             if(mid==x/mid)
+//                 return mid;
+//             else if(mid>x/mid) {
+//                 right=mid;
+//             } else {
+//                 left=mid+1;
+//             }
+//         }
+        
+//         return left-1;
+        
+        while(left<=right) {    
             mid = left + (right-left)/2;
             if(mid==x/mid)
                 return mid;
             else if(mid>x/mid) {
-                right=mid;
+                right=mid-1;
             } else {
                 left=mid+1;
             }
         }
         
-        return left-1;
+        return right;
     }
 };
