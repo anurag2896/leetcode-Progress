ea8e276d747edc8de93b362c65a4b023b9fa6ca8
Anurag Gupta <anurag281996@gmail.com>
2020-12-05 03:12:12 +0530

    188 ms

--- solutions/204. Count Primes.cpp
+++ solutions/204. Count Primes.cpp
@@ -1,23 +1,44 @@
 class Solution {
 public:
     
-    bool isPrime(int num) {
-        for(auto i=2; i*i<=num; i++) {
-            if(num%i==0)
-                return false;
-        }
-        return true;
-    }
+//     bool isPrime(int num) {
+//         for(auto i=2; i*i<=num; i++) {
+//             if(num%i==0)
+//                 return false;
+//         }
+//         return true;
+//     }
+    
+//     int countPrimes(int n) {
+//         int count=0;
+        
+//         if(n<2)
+//             return 0;
+//         for(auto i=2; i<n; i++) {
+//             if(isPrime(i))
+//                 count++;
+//         }
+//         return count;
+//     }
+    //Above complexity is O(n1.5), sieve is O(nlogn)
     
     int countPrimes(int n) {
-        int count=0;
-        
         if(n<2)
             return 0;
-        for(auto i=2; i<n; i++) {
-            if(isPrime(i))
-                count++;
+        
+        vector <bool> prime (n, true);
+        prime[0] = false;
+        prime[1] = false;
+​
+        for(auto i=0; i*i<n; i++) {
+            if(prime[i]) {
+                for(auto j=i*i; j<n; j+=i) {
+                    prime[j] = false;
+                }
+            }
         }
-        return count;
+        
+        return count(prime.begin(), prime.end(), true);;
     }
+    
 };
