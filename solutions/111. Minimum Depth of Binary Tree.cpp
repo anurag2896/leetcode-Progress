7c6f8c1469f4bd5ddf9ba57ed1612a2b4b696215
Anurag Gupta <anurag281996@gmail.com>
2021-04-23 17:27:10 +0530

    248 ms

--- solutions/111. Minimum Depth of Binary Tree.cpp
+++ solutions/111. Minimum Depth of Binary Tree.cpp
@@ -12,33 +12,34 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-        // if(!root)
-        //     return 0;
-        // return 1 + min(minDepth(root->left),minDepth(root->right));
-        queue<TreeNode*> q;
-        TreeNode* ele;
-        int level=0, n;
-        if(root) {
-            q.push(root);
-            level++;
-        }
-        while(!q.empty()) {
-            n=q.size();
-            for(int i=0; i<n; i++) {
-                ele=q.front();
-                if(ele->left) {
-                    q.push(ele->left);
-                }
-                if(ele->right){
-                    q.push(ele->right);
-                } 
-                if(!ele->left && !ele->right) {
-                    return level;
-                }
-                q.pop();
-            }
-            level++;
-        }
-        return 0;
+        if(!root) return 0;
+        if(!root->left) return 1+minDepth(root->right);
+        if(!root->right) return 1+minDepth(root->left);
+        return 1 + min(minDepth(root->left),minDepth(root->right));
+        // queue<TreeNode*> q;
+        // TreeNode* ele;
+        // int level=0, n;
+        // if(root) {
+        //     q.push(root);
+        //     level++;
+        // }
+        // while(!q.empty()) {
+        //     n=q.size();
+        //     for(int i=0; i<n; i++) {
+        //         ele=q.front();
+        //         if(ele->left) {
+        //             q.push(ele->left);
+        //         }
+        //         if(ele->right){
+        //             q.push(ele->right);
+        //         } 
+        //         if(!ele->left && !ele->right) {
+        //             return level;
+        //         }
+        //         q.pop();
+        //     }
+        //     level++;
+        // }
+        // return 0;
     }
 };
